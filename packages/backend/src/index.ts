0e9d57901086620429e4279134f877a3bfcc0390
joaopma <joaopaulo.m@terra.com.br>
2024-08-30 11:23:56 -0300

    init

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -50,5 +50,6 @@ backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
 // kubernetes
 backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
-
+backend.add(import('@backstage/plugin-auth-backend-module-microsoft-provider'));
+backend.add(import('@backstage/plugin-catalog-backend-module-msgraph/alpha'));
 backend.start();
